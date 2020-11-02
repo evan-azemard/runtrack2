2aedad31f712aa9c899d5e0190c0e9d9507a103d
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2020-11-02 17:46:26 +0100

    Job1 étape3

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -5,13 +5,16 @@
 		$str2 = "Vive";
 		$str3 = "!";
 		$val = "6";
+		$val = $val + 4;
+		$myBool = "true";
+		$myBool = $myBool + false;
 
 		$resultat = $str2 . ' ' . $str . ' ' . $str3;
 
 		echo $resultat;
 
-		$val = $val + 4;
+   		echo $val;
 
-    echo $val;
-    
+   		echo $myBool;
+
 	?>
