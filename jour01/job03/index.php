a070ca297388db009e3b22ac44afa93a19890212
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2020-11-02 18:46:26 +0100

    Job 3

---
+++ jour01/job03/index.php
@@ -0,0 +1,37 @@
+
+	<?php
+		$int = "Bonjour";
+		$chaine = "LeFeux";
+		$numberflo = 0.5;
+		$ok = True;
+	?>
+
+	<html>
+    <h1>Data types on PHP</h1>
+    <table border="1">
+    <theader>
+        <th>Type</th>
+        <th>Nom</th>
+        <th>Valeur</th>
+    </theader>
+    <tr>
+        <td>String</td>
+        <td>$chaine</td>
+        <td><?php echo($chaine);?></td>
+    </tr>
+    <tr>
+        <td>Integer</td>
+        <td>$int</td>
+        <td><?php echo($int);?></td>
+    </tr>
+    <tr>
+        <td>Boolean</td>
+        <td>$ok</td>
+        <td><?php echo($ok);?></td>
+    </tr>
+    <tr>
+        <td>Float</td>
+        <td>$numberflo</td>
+        <td><?php echo($numberflo);?></td>
+    </tr>
+    <table>
