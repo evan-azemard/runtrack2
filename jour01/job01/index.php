0439a46a4c8c0ef445582cc81d284296b2b70c0f
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2020-11-02 16:59:27 +0100

    Job1 étape 2

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -4,8 +4,14 @@
 		$str = "LaPlateforme.";
 		$str2 = "Vive";
 		$str3 = "!";
+		$val = "6";
 
 		$resultat = $str2 . ' ' . $str . ' ' . $str3;
 
 		echo $resultat;
+
+		$val = $val + 4;
+
+    echo $val;
+    
 	?>
