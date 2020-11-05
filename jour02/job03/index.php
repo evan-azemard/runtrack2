4ee07a50c685c8ab638e2d59dfbe5169d32a90c8
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2020-11-05 16:18:45 +0100

    Jour02 Jobe03

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -1,27 +1,37 @@
 
+
 <?php
 
-	$i = 1;
+	$i = 0;
 
-	while ($i < 100 {
-		
-		if ($i === 42){
-			echo ("<b><u>$i</u></b>");
+	while ($i < 101) {
+
+		if ($i > 0 and $i < 20) {
+			echo ("<i>$i</i>");
+			echo "<br/>";
+		} 
+
+		else if  ($i > 25 and $i < 50) {
+
+			if ($i == 42) {
+
+				echo ("La Plateforme_");
+				echo "<br/>";
+		}
+		else{
+			echo ("<u>$i</u>");
 			echo "<br/>";
 		}
+		}
 
-		else { 
+		else{
 			echo $i;
 			echo "<br/>";
 
 		}
-
 		$i = $i + 1;
 
-
 	}
-
-
 
 
 ?>
