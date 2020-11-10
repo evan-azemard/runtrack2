e06193beb651fdaf670b66099e61c7b5a4062a64
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2020-11-10 16:46:48 +0100

    Jour8

---
+++ jour03/Job02.php
@@ -0,0 +1,14 @@
+<?php
+
+	$str = array ("Tous ces instants seront perdus dans le temps comme les larmes sous la pluie");
+	$i = $str[0];
+	$e = $str [9];
+
+	for ($i,isset($e) == true, $i++){
+
+			echo "$str";
+}
+		}
+?>
+
+
